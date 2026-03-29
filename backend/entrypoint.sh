b1330b3681c6cd8281cb8809547acbf19054afc2
Vishwajeet Kumar <vishwajeetkumarpatelmgs@gmail.com>
2026-03-29 13:48:46 +0530

    Fix environment variable consistency and add DB migrations

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-/nakama/nakama --name nakama1 --database.address $DATABASE_URL --socket.port $PORT --session.encryption_key "defaultkey" --socket.server_key "defaultkey" --logger.level INFO --runtime.path /nakama/data/modules --runtime.js_entrypoint main.js
+# Run migrations
+/nakama/nakama migrate up --database.address $NAKAMA_DATABASE_ADDRESS
+
+# Start Nakama
+/nakama/nakama --name nakama1 --database.address $NAKAMA_DATABASE_ADDRESS --socket.port $PORT --session.encryption_key "defaultkey" --socket.server_key "defaultkey" --logger.level INFO --runtime.path /nakama/data/modules --runtime.js_entrypoint main.js
